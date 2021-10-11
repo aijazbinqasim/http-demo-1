70799753503c3f21b9b2d39e6def9bd4d6cd59de
aijazbinqasim <aijazbinqasim@hotmail.com>
2021-10-11 22:03:27 +0500

    Add http request.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+        axios.get("https://jsonplaceholder.typicode.com/posts")
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const newPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Aijaz Ali'
+                    }
+                });
+                this.setState({ posts: newPosts });
+            });
+    }
+
+    render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author} />;
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
+
                 <section>
                     <FullPost />
                 </section>
+
                 <section>
                     <NewPost />
                 </section>
